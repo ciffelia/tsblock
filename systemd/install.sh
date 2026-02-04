8e4e2a0f8dcef4fed4291e1a1beeed1c18174419
Oji Ishimori <15273128+ciffelia@users.noreply.github.com>
2026-02-05 00:47:07 +0900

    Update install and uninstall scripts

--- systemd/install.sh
+++ systemd/install.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
-
 set -euxo pipefail
-
-cd -- "$(dirname -- "${BASH_SOURCE[0]}")"
+cd "$(dirname "$0")"
 
 install ../tsblock /usr/local/sbin/tsblock
 install ./tsblock.service /etc/systemd/system/tsblock.service
--- systemd/uninstall.sh
+++ systemd/uninstall.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-
 set -euxo pipefail
 
 rm -f /usr/local/sbin/tsblock /etc/systemd/system/tsblock.service
